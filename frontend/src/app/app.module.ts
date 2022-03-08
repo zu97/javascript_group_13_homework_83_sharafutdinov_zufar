3dbcc5fa1f6bd8a9ab7107af7a2acc755bd5f89e
admin <admin@admin.com>
2022-03-08 14:07:48 +0600

    Homework-86 start. Replacing bootstrap with material design

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LayoutModule } from '@angular/cdk/layout';
 
 import { artistsReducer } from './store/artists.reducer';
 import { ArtistsEffects } from './store/artists.effects';
@@ -31,7 +34,10 @@ import { ArtistDetailsComponent } from './artists/artist-details/artist-details.
     StoreModule.forRoot({artists: artistsReducer, albums: albumsReducer}, {}),
     EffectsModule.forRoot([ArtistsEffects, AlbumsEffects]),
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
