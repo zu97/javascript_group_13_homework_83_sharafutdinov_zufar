0e1365d55818ac24473c8f4e422624fb94f4d9d2
admin <admin@admin.com>
2022-03-22 18:56:58 +0600

    Frontend route updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,12 +7,19 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { TracksComponent } from './pages/tracks/tracks.component';
 import { TracksHistoryComponent } from './pages/tracks-history/tracks-history.component';
+import { EditArtistComponent } from './pages/edit-artist/edit-artist.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { EditAlbumComponent } from './pages/edit-album/edit-album.component';
+import { EditTrackComponent } from './pages/edit-track/edit-track.component';
 
 const routes: Routes = [
   {path: '', component: ArtistsComponent},
   {path: 'artists', component: ArtistsComponent},
-  {path: 'artists/:id', component: AlbumsComponent},
-  {path: 'artists/:artist/:album', component: TracksComponent},
+  {path: 'artists/new', component: EditArtistComponent, canActivate: [AuthGuardService]},
+  {path: 'albums/:artist', component: AlbumsComponent},
+  {path: 'albums/:artist/new', component: EditAlbumComponent, canActivate: [AuthGuardService]},
+  {path: 'tracks/:album', component: TracksComponent},
+  {path: 'tracks/:album/new', component: EditTrackComponent, canActivate: [AuthGuardService]},
   {path: 'history', component: TracksHistoryComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
