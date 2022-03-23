a524894d66b7b0a8446a316b76a720f6262cef17
admin <admin@admin.com>
2022-03-23 14:45:34 +0600

    Update adding a track, if you go from the album page, the current artist and album for the frontend are immediately selected

--- frontend/src/app/pages/edit-track/edit-track.component.ts
+++ frontend/src/app/pages/edit-track/edit-track.component.ts
@@ -7,8 +7,9 @@ import { Artist } from '../../models/artist.model';
 import { fetchArtistsRequest } from '../../store/artists.actions';
 import { AddTrackData } from '../../models/track.model';
 import { addTrackRequest } from '../../store/tracks.actions';
-import { Album } from '../../models/album.model';
-import { fetchAlbumsRequest } from '../../store/albums.actions';
+import { Album, AlbumWithArtist } from '../../models/album.model';
+import { fetchAlbumsRequest, getAlbumRequest } from '../../store/albums.actions';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-track',
@@ -25,22 +26,59 @@ export class EditTrackComponent implements OnInit {
   albums: Observable<Album[]>;
   albumsLoading: Observable<boolean>;
 
+  albumId = '';
+  album: Observable<null | AlbumWithArtist>;
+
   constructor(
     private store: Store<AppState>,
+    private route: ActivatedRoute,
   ) {
     this.isLoading = store.select(state => state.tracks.addLoading);
     this.artists = store.select(state => state.artists.artists);
     this.artistsLoading = store.select(state => state.artists.fetchLoading);
     this.albums = store.select(state => state.albums.albums);
     this.albumsLoading = store.select(state => state.albums.fetchLoading);
+
+    this.album = store.select(state => state.albums.album)
   }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const albumId = <undefined | string>params['album'];
+
+      if (albumId) {
+        this.albumId = albumId;
+        this.store.dispatch(getAlbumRequest({id: albumId}));
+      } else {
+        this.setArtistValue('');
+        this.setAlbumValue('');
+      }
+    });
+
+    this.album.subscribe((album) => {
+      if (this.albumId && album) {
+        this.setArtistValue(album.artist._id);
+        this.setAlbumValue(album._id);
+      }
+    });
+
     this.store.dispatch(fetchArtistsRequest());
   }
 
-  onArtistSelected(id: string) {
-    this.store.dispatch(fetchAlbumsRequest({id}));
+  setArtistValue(value: string) {
+    setTimeout(() => {
+      this.form.form.get('artist')?.setValue(value);
+    });
+  }
+
+  setAlbumValue(value: string) {
+    setTimeout(() => {
+      this.form.form.get('album')?.setValue(value);
+    });
+  }
+
+  onArtistSelected(artistId: string) {
+    this.store.dispatch(fetchAlbumsRequest({artistId}));
   }
 
   onSubmit(): void {
