bea12896dfa976baf9f9ffc3b14a02d0b5dab188
admin <admin@admin.com>
2022-04-01 18:50:13 +0600

    Updating authorization, adding authorization through social networks for api

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,27 +1,58 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { AppState } from '../../store/types';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { NgForm } from '@angular/forms';
-import { LoginError } from '../../models/user.model';
-import { loginUserRequest } from '../../store/users.actions';
+import { LoginError, LoginSocialUserData } from '../../models/user.model';
+import { facebookLoginUserRequest, googleLoginUserRequest, loginUserRequest } from '../../store/users.actions';
+import { FacebookLoginProvider, GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
   @ViewChild('f') form!: NgForm;
 
   isLoading: Observable<boolean>;
   error: Observable<null | LoginError>;
 
+  isFacebookLoading: Observable<boolean>;
+  facebookError: Observable<null | LoginError>;
+  isGoogleLoading: Observable<boolean>;
+  googleError: Observable<null | LoginError>;
+
+  private authStateSub!: Subscription;
+
   constructor(
     private store: Store<AppState>,
+    private authService: SocialAuthService,
   ) {
     this.isLoading = store.select(state => state.users.loginLoading);
     this.error = store.select(state => state.users.loginError);
+    this.isFacebookLoading = store.select((state) => state.users.facebookLoginLoading);
+    this.facebookError = store.select((state) => state.users.facebookLoginError);
+    this.isGoogleLoading = store.select((state) => state.users.googleLoginLoading);
+    this.googleError = store.select((state) => state.users.googleLoginError);
+  }
+
+  ngOnInit(): void {
+    this.authStateSub = this.authService.authState.subscribe((user) => {
+      const userData: LoginSocialUserData = {
+        authToken: user.authToken,
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        photoUrl: user.photoUrl
+      };
+
+      if (user.provider === 'FACEBOOK') {
+        this.store.dispatch(facebookLoginUserRequest({userData}));
+      } else if (user.provider === 'GOOGLE') {
+        this.store.dispatch(googleLoginUserRequest({userData}));
+      }
+    });
   }
 
   onSubmit(): void {
@@ -30,6 +61,18 @@ export class LoginComponent {
     }
 
     this.store.dispatch(loginUserRequest({ userData: this.form.value }));
+  }
+
+  fbLogin(): void {
+    void this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  googleLogin(): void {
+    void this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  ngOnDestroy(): void {
+    this.authStateSub.unsubscribe();
   }
 
 }
