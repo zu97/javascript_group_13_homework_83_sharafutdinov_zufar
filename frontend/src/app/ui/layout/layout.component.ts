cebcb93bf75edb647cd52826669c9c72c3f323fd
admin <admin@admin.com>
2022-04-01 18:50:40 +0600

    Update logout and add social media icons for frontend

--- frontend/src/app/ui/layout/layout.component.ts
+++ frontend/src/app/ui/layout/layout.component.ts
@@ -4,6 +4,7 @@ import { User } from '../../models/user.model';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/types';
 import { logoutUserRequest } from '../../store/users.actions';
+import { SocialAuthService } from 'angularx-social-login';
 
 @Component({
   selector: 'app-layout',
@@ -15,11 +16,13 @@ export class LayoutComponent {
 
   constructor(
     private store: Store<AppState>,
+    private authService: SocialAuthService
   ) {
     this.user = store.select(state => state.users.user);
   }
 
   logout() {
+    this.authService.signOut();
     this.store.dispatch(logoutUserRequest());
   }
 
