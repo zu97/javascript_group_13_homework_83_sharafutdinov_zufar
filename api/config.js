ae2cf0dfac00e3fadcacc2053e352c74358c0673
admin <admin@admin.com>
2022-04-01 18:47:42 +0600

    Installing dependencies and setting up the config for api

--- api/config.js
+++ api/config.js
@@ -8,5 +8,13 @@ module.exports = {
     mongoConfig: {
         url: 'mongodb://localhost/hw83_zush',
         options: { useNewUrlParser: true }
+    },
+    facebook: {
+        appId: '647634579666946',
+        appSecret: '007f10c1d05afba74481f1ff3647e327'
+    },
+    google: {
+        appId: '100603535689-v40d9bfb8p4q6bamicrckn5ttcjao19j.apps.googleusercontent.com',
+        appSecret: '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'
     }
 };
